a617f4bc86795dabd2e31ca9851598e8b4a0cc4d
KKW557 <kevin557557@gmail.com>
2025-12-24 18:04:40 +0800

    2025.12.1

---
+++ src/Mod.cs
@@ -0,0 +1,70 @@
+﻿using NeoModLoader.api;
+using UnityEngine.Scripting;
+
+namespace Unlocker;
+
+public class Mod : BasicMod<Mod>
+{
+    protected override void OnModLoad()
+    {
+        var mod = GetDeclaration();
+        LogInfo("Author: " + mod.Author);
+        LogInfo("Version: " + mod.Version);
+    }
+
+    [Preserve]
+    public static void AlwaysDebugCallback(bool value)
+    {
+        DebugConfig.setOption(DebugOption.DebugButton, value);
+    }
+    
+    [Preserve]
+    public static void UnlockAllAchievements(bool value)
+    {
+        if (!value)
+        {
+            return;
+        }
+        GameProgress.instance.unlockAllAchievements();
+        _post("Unlock", "Achievements");
+    }
+    
+    [Preserve]
+    public static void ClearAllAchievements(bool value)
+    {
+        if (!value)
+        {
+            return;
+        }
+        GameProgress.instance.debugClearAllAchievements();
+        _post("Clear", "Achievements");
+    }
+    
+    [Preserve]
+    public static void UnlockAllAssets(bool value)
+    {
+        if (!value)
+        {
+            return;
+        }
+        GameProgress.instance.debugUnlockAll();
+        _post("Unlock", "Assets");
+    }
+    
+    [Preserve]
+    public static void ClearAllAssets(bool value)
+    {
+        if (!value)
+        {
+            return;
+        }
+        GameProgress.instance.debugClearAll();
+        _post("Clear", "Assets");
+    }
+
+    private static void _post(string action, string target)
+    {
+        Mod.Instance.GetConfig()["Unlocker Config Unlocker"][$"Unlocker Config {action} All {target}"].SetValue(false, true);
+        Mod.LogInfo($"{action}ed all {target}");
+    }
+}
